84e607dadd8a1e81de7dc32f6c1290adb2d5ce07
pch <changhua@changhuadeMBP.lan>
2015-06-19 15:26:22 +0800

    把抓包的数组大小减少10倍，降低负载

--- src/hello.h
+++ src/hello.h
@@ -10,7 +10,7 @@
 #define AP2C_ACK 2
 
 #define MAC_LEN			6
-#define HOLD_TIME       10000
+#define HOLD_TIME       1000
 #define CS_NUMBER       200
 #define HT_NUMBER       100
 #define PRINT_INTERVAL  20 /*20 seconds*/
